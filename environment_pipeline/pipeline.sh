05bc4036b01b6fae55d819c35e0879a863fdee48
Arthur Aguiar <72421751+aguiar-arthur@users.noreply.github.com>
2025-06-21 01:20:28 -0300

    renaming config folder

--- environment_pipeline/pipeline.sh
+++ environment_pipeline/pipeline.sh
@@ -8,16 +8,16 @@ source /.bash_functions.sh || {
 # emacs
 echo "Starting emacs setup"
 echo "1 - Creating symlink for init.el"
-create_symlink "$HOME/dotfiles/.config/doom/init.el" "$HOME/.config/doom/init.el"
+create_symlink "$HOME/dotfiles/config/doom/init.el" "$HOME/.config/doom/init.el"
 
 echo "2 - Appending files into config.el"
 CONFIG_EL="$HOME/.config/doom/config.el"
-CONFIG_EL_TEXT_TO_APPEND='(load "'"$HOME"'/dotfiles/.config/doom/config.el")'
+CONFIG_EL_TEXT_TO_APPEND='(load "'"$HOME"'/dotfiles/config/doom/config.el")'
 append_text_to_file "$CONFIG_EL" "$CONFIG_EL_TEXT_TO_APPEND"
 
 echo "3 - Appending files into packages.el"
 PACKAGES_EL="$HOME/.config/doom/packages.el"
-PACKAGES_EL_TEXT_TO_APPEND='(load "'"$HOME"'/dotfiles/.config/doom/packages.el")'
+PACKAGES_EL_TEXT_TO_APPEND='(load "'"$HOME"'/dotfiles/config/doom/packages.el")'
 append_text_to_file "$PACKAGES_EL" "$PACKAGES_EL_TEXT_TO_APPEND"
 
 echo "emacs setup finished"
@@ -28,7 +28,7 @@ echo "Starting terminal setup"
 
 echo "1 - Adding utils shell commands to the terminal source"
 ZSHRC_FILE="$HOME/.zshrc"
-ZSHRC_FILE_TEXT_TO_APPEND="source $HOME/dotfiles/.config/terminal/utils.sh"
+ZSHRC_FILE_TEXT_TO_APPEND="source $HOME/dotfiles/config/terminal/utils.sh"
 append_text_to_file "$ZSHRC_FILE" "$ZSHRC_FILE_TEXT_TO_APPEND"
 
 echo "2 - Changing zsh theme to agnoster"
