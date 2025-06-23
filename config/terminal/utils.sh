44182707155c25b971162ee6eb93e49b0bd18134
Arthur Aguiar <72421751+aguiar-arthur@users.noreply.github.com>
2025-06-23 01:31:07 -0300

    utils.sh update

--- config/terminal/utils.sh
+++ config/terminal/utils.sh
@@ -7,5 +7,35 @@ git-prune-deleted-branches() {
   git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D
 }
 
+list_files_without_word() {
+    local search_word="$1"
+
+    if [ -z "$search_word" ]; then
+        echo "Usage: list_files_without_word <word>"
+        return 1
+    fi
+
+    rg --files | while read -r file; do
+        if ! rg -q "$search_word" "$file"; then
+            echo "$file"
+        fi
+    done
+}
+
+list_files_with_word() {
+    local search_word="$1"
+
+    if [ -z "$search_word" ]; then
+        echo "Usage: list_files_with_word <word>"
+        return 1
+    fi
+
+    rg --files | while read -r file; do
+        if rg -q "$search_word" "$file"; then
+            echo "$file"
+        fi
+    done
+}
+
 # This should be the last command called
 neofetch
